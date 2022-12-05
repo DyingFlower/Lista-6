8a41c3ed3015c20f44133a4a9c604ccd539f9894
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2022-12-04 21:19:20 -0300

    Create questao2label.java

---
+++ questao2label.java
@@ -0,0 +1,12 @@
+package questao2;
+
+import javax.swing.JLabel;
+
+public class Label extends JLabel {
+	String texto;
+	public Label(String texto) {
+		this.texto=texto;
+		setText(texto);
+		setVisible(true);
+	}
+}
